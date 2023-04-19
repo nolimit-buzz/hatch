fc3823bc3b334c47573360f1b20dab3050e5ed96
segunolamide <segunolamide78@gmail.com>
2023-04-19 05:11:04 +0100

    Update html with changes

--- codd.js
+++ codd.js
@@ -4,11 +4,15 @@
     $(document).ready(function(){
 
         // Project Tab
-        $(".right-panel-menu .project-menu").each(function(i,v){
+        $(".project-menu").each(function(i,v){
         console.log('v',v);
             $(v).click(function(){
                 $active_memu = $(this).data("menu");
+                $(".project-menu").removeClass('active');
+                // $(this).toggleClass('active');
                 console.log('active_memu',$active_memu);
+          
+                $(".project-menu[data-menu='"+$active_memu+"']").toggleClass('active');
 
                 $(".right-panel-content.content").hide();
                 $(".right-panel-content.content[data-content='"+$active_memu+"']").show();
@@ -20,10 +24,15 @@
         // Project  switch
         
         $("svg#list").click(function(i,v){
+
+            $("svg#grid").removeClass('active');
+            $(this).addClass('active');
             $(".project-list").css("display","grid");
             $(".project-grid").css("display","none");;
         });
         $("svg#grid").click(function(i,v){
+            $("svg#list").removeClass('active');
+            $(this).addClass('active');
             $(".project-list").css("display","none");;
             $(".project-grid").css("display","grid");;
         });
